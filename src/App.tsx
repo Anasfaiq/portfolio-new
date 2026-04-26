6e554c1bece1ef418de02c81cbf7930f5d389d50
Anasfaiq <176678988+Anasfaiq@users.noreply.github.com>
2026-04-26 18:17:43 +0700

    fixing App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,29 +21,6 @@ const App = () => {
     }
   };
 
-  const useActiveSection = () => {
-    const [active, setActive] = useState("");
-    useEffect(() => {
-      const sections = document.querySelectorAll("section");
-
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              setActive(entry.target.id);
-            }
-          });
-        },
-        {
-          threshold: 0.6,
-        },
-      );
-      sections.forEach((section) => observer.observe(section));
-      return () => observer.disconnect();
-    }, []);
-    return active;
-  };
-
   useEffect(() => {
     const lenis = new Lenis({
       duration: 2.5,
